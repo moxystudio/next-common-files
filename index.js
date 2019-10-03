9875219cc5ce243e10a6786f57c4b452f8539507
João Silva <joao.silva@moxy.studio>
2019-10-03 16:29:24 +0100

    fix: updated snapshots

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const withRasterImages = require('./lib/raster-images');
 const withPlayback = require('./lib/playback');
 const withSVG = require('./lib/svg');
-const withFonts = require('./lib/font');
+const withFonts = require('./lib/fonts');
 
 module.exports = { withRasterImages, withPlayback, withSVG, withFonts };
