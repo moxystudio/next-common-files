e4a149a9522c4616b82600ed053ff5bd91b4ad48
André Cruz <andremiguelcruz@msn.com>
2019-10-21 21:12:05 +0100

    feat: add support for 3d files

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const withRasterImages = require('./lib/raster-images');
 const withPlayback = require('./lib/playback');
-const withSVG = require('./lib/svg');
 const withFonts = require('./lib/fonts');
+const withSVG = require('./lib/svg');
+const with3D = require('./lib/3d');
 
-module.exports = { withRasterImages, withPlayback, withSVG, withFonts };
+module.exports = { withRasterImages, withPlayback, withFonts, withSVG, with3D };
