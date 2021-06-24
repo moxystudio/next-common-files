11ebf5312fabbd087cb42808aeed80c6489c1d36
André Cruz <andremiguelcruz@msn.com>
2021-06-24 14:25:54 +0100

    feat: add json5 support
    Closes #19

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const withPlayback = require('./lib/playback');
 const withFonts = require('./lib/fonts');
 const withSVG = require('./lib/svg');
 const with3D = require('./lib/3d');
+const withJSON5 = require('./lib/json5');
 
-module.exports = { withRasterImages, withPlayback, withFonts, withSVG, with3D };
+module.exports = { withRasterImages, withPlayback, withFonts, withSVG, with3D, withJSON5 };
